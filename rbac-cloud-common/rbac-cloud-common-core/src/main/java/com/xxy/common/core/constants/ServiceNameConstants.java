bc7346fb345bd21f3a008b7addac3f8bcb4b798a
xiangxy26878 <xiangxy26878@hundsun.com>
2020-01-12 03:04:23 +0800

    前端页面提交

---
+++ rbac-cloud-common/rbac-cloud-common-core/src/main/java/com/xxy/common/core/constants/ServiceNameConstants.java
@@ -0,0 +1,14 @@
+package com.xxy.common.core.constants;
+
+public interface ServiceNameConstants {
+	/**
+	 * 认证服务的SERVICEID
+	 */
+	String AUTH_SERVICE = "pig-auth";
+
+	/**
+	 * UMPS模块
+	 */
+	String UMPS_SERVICE = "pig-upms-biz";
+
+}
